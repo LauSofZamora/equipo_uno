08730f8ec4918acb70e76d50c65394004377487d
Laura Sofia Zamora <zamora.laura@correounivalle.edu.co>
2024-11-24 14:46:56 -0500

    Cambio de la versión del SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.equipo_1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.equipo_1"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
